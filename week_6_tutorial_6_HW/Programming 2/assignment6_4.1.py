f4a149a6a0b41717c4eddf4107a55403d9860f22
Maryna Nemyrovska <maryna.nemyrovska@gmail.com>
2019-06-11 16:22:47 +0200

    Polynomial chaos via chaospy

--- week_6_tutorial_6_HW/Programming 2/assignment6_4.1.py
+++ week_6_tutorial_6_HW/Programming 2/assignment6_4.1.py
@@ -37,36 +37,34 @@ if __name__ == '__main__':
     x_axis = np.linspace(0.0, t_max, num = grid_len,  endpoint=True)
     sol_odeint_true = discretize_oscillator_odeint(model, init_cond, x_axis, params_odeint, atol, rtol)
 
-    N = [2,4,6]
-    K = [1,2,3]
+    K = [2,4,6] # for approximation
+    N = [1,2,3] # for expansion coefficients
 
     mu, V = np.zeros(len(N)), np.zeros(len(N))
 
+    distr_unif_w = cp.Uniform(0.95, 1.05)
 
+    for i, n in enumerate(N):
+        # generate K Gaussian nodes and weights based on normal distr (we need to appr with quadratures)
+        # appr with gaussian polynomials
+        nodes, weights = cp.generate_quadrature(K[i], distr_unif_w, rule = "G") #nodes [[1,2]]
+        orth_poly = cp.orth_ttr(N[i], distr_unif_w, normed = True)
+        #evaluate f(x) at all quadrature nodes and take the y(10), i.e [-1]
+        y_out = [discretize_oscillator_odeint(model, init_cond, x_axis, (c, k, f, node), atol, rtol)[-1] for node in nodes[0]]
 
-    for i, n in enumerate(M):
-        distr = cp.Uniform(0.95, 1.05)
-        w_generated = distr.sample(size=n)
-        outputs_MC_y= [] #y0
+        #expect_y = np.sum(y_out * weights)
 
-        print("Calculating ... ", n)
+        # find generalized Polynomial chaos and expansion coefficients
+        gPC_m, expansion_coeff = cp.fit_quadrature(orth_poly, nodes, weights, y_out, retall = True)
+        # gPC_m is the polynomial that approximates the most
 
-        output_interpol =  []
-        for w_value in w_generated:
-            # direct MC sampling
-            params_odeint_new = c, k, f, w_value
-            sol_odeint = discretize_oscillator_odeint(model, init_cond, x_axis, params_odeint_new, atol, rtol)
-            outputs_MC_y.append(sol_odeint[-1])
+        print(f'The best polynomial of degree {n} that approximates f(x): {cp.around(gPC_m, 1)}')
+        print(f'Expansion coeff [0] = {expansion_coeff[0]}')#, expect_weights: {expect_y}')
 
+        mu = cp.E(gPC_m, distr_unif_w)
+        V = cp.Var(gPC_m, distr_unif_w)
 
-        mu[i] = np.mean(np.array(outputs_MC_y))
-        V[i] = np.var(np.array(outputs_MC_y), ddof  =1)
-
-
-    print("Generated mean and variance:")
-    for i in range(mu.shape[0]):
-        print("M = %6d" % M[i], "mean :", "%.3f" % (mu[i]))
-        print("\t\t\t var :%.6f" % (V[i]))
+        print("mu = %.3f,V = %.3f" %(mu, V))
 
 
     mu_ref = [-0.43893703]
@@ -77,5 +75,5 @@ if __name__ == '__main__':
     rel_err_V = np.abs(1 - V / V_ref)
     #cols - grid points
     #rows - sampling
-    rel_err_mu_interpol = np.abs(1 - mu_interpol / mu_ref).T
-    rel_err_V_interpol = np.abs(1 - V_interpol/ V_ref).T
+    #rel_err_mu_interpol = np.abs(1 - mu_interpol / mu_ref).T
+    #rel_err_V_interpol = np.abs(1 - V_interpol/ V_ref).T
