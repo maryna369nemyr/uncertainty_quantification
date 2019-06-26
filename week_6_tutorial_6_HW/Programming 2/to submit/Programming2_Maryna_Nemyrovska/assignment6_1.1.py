9f266c85e69a285851790b11a6c606ea30e8f54d
Maryna Nemyrovska <maryna.nemyrovska@gmail.com>
2019-06-26 10:50:50 +0200

    Folder to submit programming 2

---
+++ week_6_tutorial_6_HW/Programming 2/to submit/Programming2_Maryna_Nemyrovska/assignment6_1.1.py
@@ -0,0 +1,185 @@
+import numpy as np
+import chaospy as cp
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import random
+import time
+
+# to use the odeint function, we need to transform the second order differential equation
+# into a system of two linear equations
+def model(init_cond, t, params):
+    z0, z1 = init_cond
+    c, k, f, w = params
+    f = [z1, f * np.cos(w * t) - k * z0 - c * z1]
+    return f
+
+def discretize_oscillator_odeint(model, init_cond, t, args, atol, rtol):
+    sol = odeint(model, init_cond, t, args=(args,), atol=atol, rtol=rtol)
+    return sol[:, 0] # only y_0
+
+def compute_barycentric_weights(grid):
+    size    = len(grid)
+    w       = np.ones(size)
+
+    for j in range(1, size):
+        for k in range(j):
+            diff = grid[k] - grid[j]
+
+            w[k] *= diff
+            w[j] *= -diff
+
+    for j in range(size):
+        w[j] = 1./w[j]
+
+    return w
+
+# rewrite Lagrange interpolation in the first barycentric form
+def barycentric_interp(eval_point, grid, func_eval, weights):
+    interp_size = len(func_eval)
+    L_G         = 1.
+    res         = 0.
+
+    for i in range(interp_size):
+        L_G   *= (eval_point - grid[i])
+
+    for i in range(interp_size):
+        if abs(eval_point - grid[i]) < 1e-10:
+            res = func_eval[i]
+            L_G    = 1.0
+            break
+        else:
+            res += (weights[i]*func_eval[i])/(eval_point - grid[i])
+
+    res *= L_G
+
+    return res
+
+
+def interpolation(cheb_grids, weights_grid, func_evals, w_eval):
+   # return an array with columns - grid_size
+    output_interpolation = np.zeros(len(cheb_grids))
+    i= 0
+    for cheb_grid, weights, func_eval in zip(cheb_grids, weights_grid, func_evals):
+        output_interpolation[i] = barycentric_interp(w_eval, cheb_grid, func_eval, weights)
+        i+=1
+    return output_interpolation
+
+
+if __name__ == '__main__':
+    c,k ,f, w, y0, y1 = 0.5, 2.0, 0.5, 1.0, 0.5, 0.
+
+    # time domain setup
+    t_max, dt  = 10., 0.01
+
+    # arguments setup for calling the three discretization functions
+    params = c, k, f, w, y0, y1
+    init_cond = y0, y1
+    params_odeint = c, k, f, w
+
+    # relative and absolute tolerances for the ode int solver
+    atol , rtol= 1e-10, 1e-10
+
+    # define uniform interpolation's grid size
+    N = [5, 10, 20]
+    grid_N = [6, 11, 21] # points should on one more than N - degree of the polynomial
+
+    grid_len = int(t_max / dt) + 1
+    x_axis = np.linspace(0.0, t_max, num=grid_len,  endpoint=True)
+
+    M = [10, 100, 1000]#, 10000]
+    mu, V = np.zeros(len(M)), np.zeros(len(M))
+    mu_interpol, V_interpol = np.zeros(((len(M)), len(grid_N))), np.zeros((len(M),len(grid_N)))
+    #columns - number of interpolation points
+    #rows - number of sampled points
+
+
+    begin = 0.95
+    end = 1.05
+
+
+    ### Precalculation for interpolation method
+
+    # grid from 0 to t_max, minus - from 0 to t_max not from t_max to 0
+    cheb_grids = [np.array(
+        [0.5 * (end + begin) - 0.5 * (end - begin) * np.cos((2 * j - 1) / (2. * grid_size) * np.pi) for j in range(1, grid_size + 1)])
+                  for grid_size in grid_N]  # len(grid_N)
+    weights_grid = [compute_barycentric_weights(grid) for grid in cheb_grids]
+    func_evals =[]
+
+    for grid in cheb_grids:
+        func_result_eval = np.array(
+            [discretize_oscillator_odeint(model, init_cond, x_axis, (c, k, f, grid_point), atol, rtol)[-1]
+             for grid_point in grid])
+        func_evals.append(func_result_eval)
+    # The end of precalculation
+
+    for i, n in enumerate(M):
+        distr = cp.Uniform(0.95, 1.05)
+        w_generated = distr.sample(size=n)
+        outputs_MC_y= [] #y0
+
+        print("Calculating ... ", n)
+
+        now_mc = time.time()
+        for w_value in w_generated:
+            params_odeint_new = c, k, f, w_value
+            # direct MC sampling
+            sol_odeint = discretize_oscillator_odeint(model, init_cond, x_axis, params_odeint_new, atol, rtol)
+            outputs_MC_y.append(sol_odeint[-1])
+        print(f'Time for {n} generated values using MC sampling {time.time() - now_mc}')
+
+        now_interpol = time.time()
+        output_interpol = []
+        for w_value in w_generated:
+            # using_interpolation
+            output_interpol.append(interpolation(cheb_grids, weights_grid, func_evals, w_value))
+        print(f'Time for {n} generated values using interpolation {time.time() - now_interpol}')
+        output_interpol = np.array(output_interpol)
+
+
+        mu_interpol[i] = np.mean(output_interpol, axis = 0)
+        V_interpol[i]= np.var(output_interpol,axis =0 , ddof = 1)
+        print("mean_interpol:", (mu_interpol[i]))
+        print("var_interpol: ",(V_interpol[i]))
+
+        mu[i] = np.mean(np.array(outputs_MC_y))
+        V[i] = np.var(np.array(outputs_MC_y), ddof  =1)
+
+
+    print("Mean and variance obtained from the direct MC sampling:")
+    for i in range(mu.shape[0]):
+        print("M = %6d" % M[i], "mean :", "%.3f" % (mu[i]))
+        print("\t\t\t var :%.6f" % (V[i]))
+
+
+    mu_ref = [-0.43893703]
+    V_ref  = [0.00019678]
+
+    #cols -sampling M
+    rel_err_mu = np.abs(1 - mu / mu_ref)
+    rel_err_V = np.abs(1 - V / V_ref)
+    #cols - grid points
+    #rows - number of samples
+    rel_err_mu_interpol = np.abs(1 - mu_interpol / mu_ref).T
+    rel_err_V_interpol = np.abs(1 - V_interpol/ V_ref).T
+
+    print(rel_err_mu)
+    print(rel_err_mu_interpol)
+    print(rel_err_V,rel_err_V_interpol)
+
+    print("Relative error tables for the mean")
+    print("%7s  %10s  %10s  %10s  %10s" %("samples","MC", "grid 6", "grid 11", "grid 21"))
+    for i in range(len(M)):
+        print("%7d" % M[i], " %.8f  %.8f  %.8f  %.8f" % (rel_err_mu[i],
+                                                      rel_err_mu_interpol[0][i],
+                                                      rel_err_mu_interpol[1][i],
+                                                      rel_err_mu_interpol[2][i]))
+
+    print("Relative error tables for the variance")
+    print("%7s  %10s  %10s  %10s  %10s" %("samples","MC", "grid 6", "grid 11", "grid 21"))
+    for i in range(len(M)):
+        print("%7d" % M[i], " %.8f  %.8f  %.8f  %.8f" % (rel_err_V[i],
+                                                      rel_err_V_interpol[0][i],
+                                                      rel_err_V_interpol[1][i],
+                                                      rel_err_V_interpol[2][i]))
+
---
+++ week_6_tutorial_6_HW/Programming 2/to submit/Programming2_Maryna_Nemyrovska/assignment6_4.1.py
@@ -0,0 +1,157 @@
+import numpy as np
+import chaospy as cp
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import random
+import time
+
+
+# to use the odeint function, we need to transform the second order differential equation
+# into a system of two linear equations
+def model(init_cond, t, params):
+    z0, z1 = init_cond
+    c, k, f, w = params
+    f = [z1, f * np.cos(w * t) - k * z0 - c * z1]
+    return f
+
+def discretize_oscillator_odeint(model, init_cond, t, args, atol, rtol):
+    sol = odeint(model, init_cond, t, args=(args,), atol=atol, rtol=rtol)
+    return sol[:, 0] # only y_0
+
+def plot_rel_errors(N, rel_error1, rel_error2, label_name1, label_name2, loglog_bool =True):
+    plt.figure()
+    if(loglog_bool):
+        plt.loglog(N, rel_error1, 'ro', label=label_name1)
+        plt.loglog(N, rel_error2, 'gx', label=label_name2)
+        plt.xlabel('Number of samples (loglog)')
+    else:
+        plt.plot(N, rel_error1, 'ro', label=label_name1)
+        plt.plot(N, rel_error2, 'gx', label=label_name2)
+        plt.xlabel('The degree of polynomial N')
+    plt.legend(loc='best', fontsize=8)
+    plt.ylabel('Error values')
+
+def plot_rel_error(N, rel_error, label_name, xlabel_name, line_type, loglog_bool =True):
+    plt.figure()
+    if(loglog_bool):
+        plt.loglog(N, rel_error, line_type, label=label_name)
+        plt.xlabel('Number of samples (loglog)')
+    else:
+        plt.plot(N, rel_error, line_type, label=label_name)
+        plt.xlabel(xlabel_name)
+    plt.legend(loc='best', fontsize=8)
+    plt.ylabel('Error values')
+
+def get_quantity_of_interest(expansion_coefficients):
+    mean = expansion_coefficients[0]
+    var = 0
+    for i in range(1, len(expansion_coefficients)):
+            var+= expansion_coefficients[i] **2
+    return(mean, var)
+
+if __name__ == '__main__':
+    c,k ,f, w, y0, y1 = 0.5, 2.0, 0.5, 1.0, 0.5, 0.
+
+    # time domain setup
+    t_max, dt  = 10., 0.01
+
+    # arguments setup for calling the three discretization functions
+    params = c, k, f, w, y0, y1
+    init_cond = y0, y1
+    params_odeint = c, k, f, w
+
+    # relative and absolute tolerances for the ode int solver
+    atol , rtol= 1e-10, 1e-10
+
+    grid_len = int(t_max / dt) + 1
+    x_axis = np.linspace(0.0, t_max, num = grid_len,  endpoint=True)
+    sol_odeint_true = discretize_oscillator_odeint(model, init_cond, x_axis, params_odeint, atol, rtol)
+
+    K = [2,4,6]#, 8, 10] # for approximation
+    N = [1,2,3]#, 4, 5] # for expansion coefficients
+
+
+    mu, V = np.zeros(len(N)), np.zeros(len(N))
+    mu_expansion, V_expansion= np.zeros(len(N)), np.zeros(len(N))
+
+    distr_unif_w = cp.Uniform(0.95, 1.05)
+    orth_polies = []
+    for i, n in enumerate(N):
+        # generate K Gaussian nodes and weights based on normal distr (we need to appr with quadratures)
+        nodes, weights = cp.generate_quadrature(K[i], distr_unif_w, rule = "G") #nodes [[1,2]]
+        # NOTE: for k == 2 it generates 3 nodes
+
+        # approximating with gaussian polynomials ( for N == 1 at gives a polynomials up to a degree 1 => 2 polynomials)
+        orth_poly = cp.orth_ttr(N[i], distr_unif_w, normed = True)
+
+        #evaluate f(x) at all quadrature nodes and take the y(10), i.e [-1]
+        y_out = [discretize_oscillator_odeint(model, init_cond, x_axis, (c, k, f, node), atol, rtol)[-1] for node in nodes[0]]
+
+        # find generalized Polynomial chaos and expansion coefficients
+        gPC_m, expansion_coeff = cp.fit_quadrature(orth_poly, nodes, weights, y_out, retall = True)
+        #gPC_m = cp.fit_quadrature(orth_poly, nodes, weights, y_out)
+
+        # gPC_m is the polynomial that approximates the most
+        print(f'Expansion coeff chaospy: {expansion_coeff}')
+        print(f'The best polynomial of degree {n} that approximates f(x): {cp.around(gPC_m, 1)}')
+        print(f'Expansion coeff [0] = {expansion_coeff[0]}')#, expect_weights: {expect_y}')
+
+        mu[i] = cp.E(gPC_m, distr_unif_w)
+        V[i]= cp.Var(gPC_m, distr_unif_w)
+
+        print("mu = %.8f,V = %.8f" % (mu[i], V[i]))
+
+
+# manual calculation of the expansion coefficients
+#Note if you do it in the same loop, the mean results changing only due to the fact that we do the loop over K[i] without any action
+    print("____________Manual expansion coefficients__________")
+
+    for i, n in enumerate(N):
+        # generate K Gaussian nodes and weights based on normal distr (we need to appr with quadratures)
+        nodes, weights = cp.generate_quadrature(K[i], distr_unif_w, rule="G")  # nodes [[1,2]]
+
+        # appr with gaussian polynomials
+        orth_poly = cp.orth_ttr(N[i], distr_unif_w, normed=True)
+
+        # evaluate f(x) at all quadrature nodes and take the y(10), i.e [-1]
+        y_out = [discretize_oscillator_odeint(model, init_cond, x_axis, (c, k, f, node), atol, rtol)[-1] for node in
+                 nodes[0]]
+
+        #print(nodes, weights)
+
+        expansion_coeff_manual = np.zeros(len(orth_poly))
+        # manual expansion coefficients
+        for j in range(len(orth_poly)):
+            for l in range(len(nodes[0])):
+
+                expansion_coeff_manual[j] += y_out[l] * orth_poly[j](nodes[0][l])* weights[l]
+
+        mu_expansion[i], V_expansion[i] = get_quantity_of_interest(np.array(expansion_coeff_manual))
+        print(f'Expansion coeff for N = {n}:\n {expansion_coeff_manual}')
+        print("mu_exp = %.8f,V_exp = %.8f" %( mu_expansion[i], V_expansion[i]))
+
+    mu_ref = [-0.43893703]
+    V_ref  = [0.00019678]
+
+    #cols -sampling M
+    rel_err_mu = np.abs(1 - mu / mu_ref)
+    rel_err_V = np.abs(1 - V / V_ref)
+    rel_err_mu_exp = np.abs(1 - mu_expansion / mu_ref)
+    rel_err_V_exp = np.abs(1 - V_expansion/ V_ref)
+
+    #plot_rel_errors(N, rel_err_mu, rel_err_mu_exp, "mean chaospy", "mean exp coeff", False)
+    #plot_rel_errors(N, rel_err_V, rel_err_V_exp, "var chaospy", "var exp coeff", False)
+    #plt.show()
+
+    print("Relative error tables for the mean")
+    print("%17s  %10s  %10s" %("max degree of poly","chaospy", "manual"))
+    for i in range(len(N)):
+        print("%18d" % N[i], " %.8f  %.8f" % (rel_err_mu[i],
+                                                      rel_err_mu_exp[i]))
+
+    print("Relative error tables for the variance")
+    print("%17s  %10s  %10s" % ("max degree of poly", "chaospy", "manual"))
+    for i in range(len(N)):
+        print("%18d" % N[i], " %.8f  %.8f" % (rel_err_V[i],
+                                                      rel_err_V_exp[i]))
+
