c7a1e03702eb18adc3141f7627753ea26f4bf310
Maryna Nemyrovska <maryna.nemyrovska@gmail.com>
2019-06-26 11:22:17 +0200

    Uniform and fixed func ass8_3

--- week_78_tutorial_78/assignment8_3.py
+++ week_78_tutorial_78/assignment8_3.py
@@ -3,24 +3,24 @@ import numpy as np
 
 
 def func_test(x1,x2,x3,x4,x5):
-    out = x1*x2*x3 + np.sin(x2+ x4) - np.exp(-x5)*x1 + x5 - x1
+    out = x1*x2*x3 + np.sin(x2+ x4) - np.exp(-x5)*x1 + x5**2 - x1
     return out
 
 if __name__ == '__main__':
     K = [3,4,5]
-    normal_distr1 = cp.Normal(0,1)
-    normal_distr2 = cp.Normal(0, 1)
-    normal_distr3 = cp.Normal(0, 1)
-    normal_distr4 = cp.Normal(0, 1)
-    normal_distr5= cp.Normal(0, 1)
-    normal_distr_multivariate = cp.J(normal_distr1, normal_distr2, normal_distr3, normal_distr4, normal_distr5)
+    unif_distr1 = cp.Uniform()
+    unif_distr2 = cp.Uniform()
+    unif_distr3 = cp.Uniform()
+    unif_distr4 = cp.Uniform()
+    unif_distr5= cp.Uniform()
+    unif_distr_multivariate = cp.J(unif_distr1, unif_distr2, unif_distr3, unif_distr4, unif_distr5)
     print("With spearse grids:")
     print("_____________________")
     for i, k in enumerate(K):
         # we always generate Gaussian quadratures
         # for normal -  hermit
         #for uniform - Legendre ort polynomials
-        nodes, weights = cp.generate_quadrature(k, normal_distr_multivariate, rule = 'G', sparse = True)
+        nodes, weights = cp.generate_quadrature(k, unif_distr_multivariate, rule = 'G', sparse = True)
         #for Gaussian quadratures we have k =N
         # since we are in 5 dim case we need to generate k points in 5
         # here it is automatically
@@ -30,7 +30,7 @@ if __name__ == '__main__':
         x1,x2,x3,x4,x5 = nodes
 
 
-        poly = cp.orth_ttr(k, normal_distr_multivariate, normed = True)
+        poly = cp.orth_ttr(k, unif_distr_multivariate, normed = True)
 
         y_out = func_test(x1,x2,x3,x4,x5)
 
@@ -41,7 +41,7 @@ if __name__ == '__main__':
         # gPC_m is the polynomial that approximates the most
         print(f'Expansion coeff [0] = {expansion_coeff[0]}')#, expect_weights: {expect_y}')
 
-        mu = cp.E(gPC_m, normal_distr_multivariate)
+        mu = cp.E(gPC_m, unif_distr_multivariate)
         print(f'Mean value from gPCE: {mu}')
 
 
@@ -53,7 +53,7 @@ if __name__ == '__main__':
         # we always generate Gaussian quadratures
         # for normal -  hermit
         #for uniform - Legendre ort polynomials
-        nodes, weights = cp.generate_quadrature(k, normal_distr_multivariate, rule = 'G', sparse = False)
+        nodes, weights = cp.generate_quadrature(k, unif_distr_multivariate, rule = 'G', sparse = False)
         #for Gaussian quadratures we have k =N
         # since we are in 5 dim case we need to generate k points in 5
         # here it is automatically
@@ -62,8 +62,8 @@ if __name__ == '__main__':
         print(nodes.shape)
         x1,x2,x3,x4,x5 = nodes
 
-
-        poly = cp.orth_ttr(k, normal_distr_multivariate, normed = True)
+        # here we have unif polynomials since we get integral from 0 to 1 with density equal to 1
+        poly = cp.orth_ttr(k, unif_distr_multivariate, normed = True)
 
         y_out = func_test(x1,x2,x3,x4,x5)
 
@@ -74,7 +74,7 @@ if __name__ == '__main__':
         # gPC_m is the polynomial that approximates the most
         print(f'Expansion coeff [0] = {expansion_coeff[0]}')#, expect_weights: {expect_y}')
 
-        mu = cp.E(gPC_m, normal_distr_multivariate)
+        mu = cp.E(gPC_m, unif_distr_multivariate)
         print(f'Mean value from gPCE: {mu}')
 
 
