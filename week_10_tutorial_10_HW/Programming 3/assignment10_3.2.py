2e573d146384790624d916de85b4b9db4354643a
Maryna Nemyrovska <maryna.nemyrovska@gmail.com>
2019-07-06 14:16:44 +0200

    Assignment 10 3.2 in progress

--- week_10_tutorial_10_HW/Programming 3/assignment10_3.2.py
+++ week_10_tutorial_10_HW/Programming 3/assignment10_3.2.py
@@ -20,6 +20,7 @@ def discretize_oscillator_euler(t, dt, params):
 
 
 def wiener_process(t_vector, f_mean):
+    #when we have deviationg in dt liek 0.01 etc
     t_vector_from = t_vector
     t_vector_from = np.insert(t_vector, len(t_vector_from), 0)
     t_vector_what = np.insert(t_vector, 0, 0)
@@ -30,6 +31,13 @@ def wiener_process(t_vector, f_mean):
     W = np.cumsum(dW)
     W = np.insert(W, 0, f_mean)
     return W
+
+def wiener_process_dt(dt, f_mean, N):
+    dW = np.sqrt(dt) * np.random.normal(0,1, N - 1)
+    W = np.cumsum(dW)
+    W = np.insert(W, 0, f_mean)
+    return W
+
 
 def plot_wiener(wiener, t_axis, show = True):
     plt.figure("Wiener process std")
@@ -62,34 +70,52 @@ def plot_processes_via_expnsions(M, t, output_processes, show = True):
         plt.show()
 
 if __name__ == '__main__':
-    N = 1000
-    M_lambda = 1000
-    M = [5,10,100]
     np.random.seed(120)
-    generated_samples  = np.random.normal(0, 1, max(M))
-
-
+    #parameters setup
+    c = 0.5
+    k = 2.0
+    y0 = 0.5
+    y1 = 0.
+    w = 1.0
+    f_mean = 0.5
 
     #Eigen values and eigen vectors for Wiener process
     eigen_values  = lambda x, T_max:  T_max**2 / (((x + 0.5)**2) * (np.pi**2))
     eigen_vectors = lambda x, t, T_max: np.sqrt(2/T_max)* np.sin((x+ 0.5)*np.pi * t/ T_max)
 
-    f_mean = 0.5
-
+    #time domain
     t_max  = 10
     dt  = 0.01
     num_t  = t_max // dt +1
-    t = np.linspace(start = 0, stop  = t_max, num = num_t)
+    t = np.round(np.linspace(start = 0, stop  = t_max, num = num_t), int(np.abs(np.log10(dt))) + 1)
     N_t_axis = len(t)
     print("here=", t, N_t_axis)
 
     wiener_f = wiener_process(t, f_mean)
-    plot_wiener(wiener_f, t)
+    plot_wiener(wiener_f, t) #gives the same result with a fixed seed
+
+    wiener_2f = wiener_process_dt(dt, f_mean, N_t_axis)
+    plot_wiener(wiener_2f, t) # gives the same result with a fixe sid
+
+
+    n_mc = 1000
+
+
+    for i in range(n_mc):
+        # for each monte carlo sampling
+        params =c, k, f_mean, w, y0, y1
+
+        discretize_oscillator_euler(t, dt, params)
 
     # Karhunen Loeve expansion
+    M = [5, 10, 100]
+
+    generated_samples = np.random.normal(0, 1, max(M))
+
     output_processes = np.zeros((len(M), len(t)))
     # rows M=10, ... M= 100
     # columns time [0 .. 1]
+
     for i, m in enumerate(M):
         for j, t_point in enumerate(t):
             output_processes[i][j] = karhunen_loeve_expansion(generated_samples, m, t_point, t_max)
