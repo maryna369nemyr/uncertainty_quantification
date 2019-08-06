Fixed bug in KL expansion and damped oscillaor. Assignment 10

week_10_tutorial_10_HW/Programming 3/assignment10_3.1.py
@@ -66,7 +66,7 @@ def karhunen_loeve_expansion(generated_samples, m, t_point):
 
 
 if __name__ == '__main__':
-    N = 1000
+    N = 100
     M_lambda = 1000
     M = [10,100,1000]
     np.random.seed(120)

week_10_tutorial_10_HW/Programming 3/assignment10_3.2_v2_rename_old.py
@@ -126,8 +126,8 @@ if __name__ == '__main__':
     f_mean = 0.5
 
     #Eigen values and eigen vectors for Wiener process
-    eigen_values  = lambda x, T_max:  (T_max / ((x + 0.5) * np.pi))**2
-    eigen_vectors = lambda x, t, T_max: np.sqrt(2/T_max)* np.sin(((x+ 0.5)*np.pi * t)/ T_max)
+    eigen_values  = lambda x, T_max:  (T_max / ((x - 0.5) * np.pi))**2
+    eigen_vectors = lambda x, t, T_max: np.sqrt(2/T_max)* np.sin(((x- 0.5)*np.pi * t)/ T_max)
 
     #time domain
     t_max  = 10